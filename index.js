c072b6573fa0c3d351a1ee147fa28239f0f978c9
Foundy <foundy@foundy.io>
2017-03-17 11:07:15 +0900

    Changed the function

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict';
 
-module.exports = function (str) {
-  return str.length;
+module.exports = function (by) {
+  var span = document.createElement('span');
+  span.setAttribute('style', 'color:ray');
+  span.textContent = 'Component : workflow-test-common (by ' + by + ')';
+
+  return span;
 };
